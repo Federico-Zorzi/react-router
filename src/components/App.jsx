aedee5c40e8adf1c3c7aeb12c3509f21d52c886b
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-12-10 12:45:58 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+/* import { useState } from "react"; */
+
+//* import layout components
+import Header from "./layout/Header";
+import Main from "./layout/Main";
+import Footer from "./layout/Footer";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <main>
+      <div className="container">
+        <p>Main</p>
+      </div>
+    </main>
+  );
+}
